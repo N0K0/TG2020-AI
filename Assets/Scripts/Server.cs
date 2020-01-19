c2d1c7aa34e1af36975319b9c40e64752f490d4e
Nikolas Papaioannou <nikolas.papaioannou@gmail.com>
2020-01-19 22:20:38 +0100

    Init work on ws server
    Init work on ws server

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -3,12 +3,48 @@ using System.Collections;
 using WebSocketSharp;
 using WebSocketSharp.Server;
 
+
+public class SocketServer : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Debug.Log(e.ToString());
+    }
+
+    protected override void OnOpen()
+    {
+        Debug.Log("New connection open!");
+    }
+
+    protected override void OnClose(CloseEventArgs e)
+    {
+        Debug.Log(e.Reason);
+    }
+
+    protected override void OnError(ErrorEventArgs e)
+    {
+        Debug.LogError(e.Message);
+    }
+
+}
+
 public class Server : MonoBehaviour
 {
-
+    private SocketServer socketServer;
+    private WebSocketServer wss;
+    
     void Awake()
     {
-        print("test");
-        WebSocketServer wss = new WebSocketServer("ws://localhost:8888");
+        wss = new WebSocketServer("ws://localhost:8888");
+        wss.AddWebSocketService<SocketServer>("/Server");
+        wss.Start();
+    }
+
+    void Update()
+    {
+        if(wss.IsListening)
+        {
+            Debug.Log("WSS is Running");
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # The Gathering 2020 AI competition: Gaia Edition
 
 The SpaceCrew wants to save money on training personnel while also maintaining the same military prowess.
-Quite a bit of discussion later the SpaceCrew figured that they should try using some sort of AI for controlling the smaller ships the brought along.
-After the event with Mother in 2019 the SpaceCrew do not want to use any of the existing General purpose AI Technology they have invented.
+Quite a bit of discussion later the SpaceCrew figured that they should try using some sort of AI for controlling the smaller ships they brought along.
+After the event with Mother in 2019 the SpaceCrew do not want to use any of the existing General purpose AI Technology they have invented. No need to lose yet an other planet to some "helpful" robot.
 This is where you come in, your mission is to start the work on an specialized AI that should be able to control our spaceships hopefully without ending with us having to escape from GAIA too..
 
 ## Intro to the challange
